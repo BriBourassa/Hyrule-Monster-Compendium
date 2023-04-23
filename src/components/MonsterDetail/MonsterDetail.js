b0b4becf68374bd59cb9cabd68ca0ebacf7eed9e
BriBourassa <111149043+BriBourassa@users.noreply.github.com>
2023-04-22 21:23:06 -0600

    Add Zelda font, background image

--- src/components/MonsterDetail/MonsterDetail.js
+++ src/components/MonsterDetail/MonsterDetail.js
@@ -1,15 +1,29 @@
 import React from "react";
-import './MonsterDetail.css'
+import "./MonsterDetail.css";
 
 const MonsterDetail = ({ monster, favoriteMonster }) => {
-    const {name, image, description, drops, common_locations} = monster
+  const { name, image, description, drops, common_locations } = monster
+  const styles = {}
   return (
     <div className="monster-detail-view">
       <h1>{name.toUpperCase()}</h1>
       <img src={image} />
       <p>{description}</p>
-      {(common_locations && common_locations.length > 0) && <p>Common Locations:{common_locations.join(', ')}</p>}
-      {(drops && drops.length > 0) && <p>Drops:{drops.join(', ')}</p>}
+
+      {common_locations && common_locations.length > 0 && (
+        <dl>
+          <dt>Common Locations:</dt>
+          <dd>{common_locations.join(", ")}</dd>
+        </dl>
+      )}
+
+      {drops && drops.length > 0 && (
+        <dl>
+          <dt>Drops:</dt>
+          <dd>{drops.join(", ")}</dd>
+        </dl>
+      )}
+
       <button onClick={() => favoriteMonster(monster)}>Favorite!</button>
     </div>
   );
